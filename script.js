e2c970e12873dd9aef5a9ad814fa3eb3bad23608
Nicolás Heredia <nicolasheredia02@gmail.com>
2022-08-15 14:37:41 -0300

    Reestructuración de página

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onscroll = function(){
+    AddOrRemove()
+};
+
+let Nav = document.getElementById("inicio");
+let NavPosition = Nav.offsetTop;
+
+function AddOrRemove(){
+    if(window.pageYOffset >= 650){
+        Nav.classList.add("fixed");
+    }
+    else{
+        Nav.classList.remove("fixed");
+    // else if (window.pageXOffset < 700) {
+
+    // }
+    
+}}
